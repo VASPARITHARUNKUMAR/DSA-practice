bbb66149cf146de6ec34d488107aa9de81932018
VASPARITHARUNKUMAR <103122665+VASPARITHARUNKUMAR@users.noreply.github.com>
2023-08-28 21:15:13 +0530

    Add files via upload

---
+++ trapping rain water.py
@@ -0,0 +1,20 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left=0
+        right=len(height)-1
+        max_left=max_right=0
+        trapped_water=0
+        while left<right:
+            if height[left]<height[right]:
+                if height[left]>max_left:
+                    max_left=height[left]
+                else:
+                    trapped_water+=max_left-height[left]
+                left+=1
+            else:
+                if height[right]>max_right:
+                    max_right=height[right]
+                else:
+                    trapped_water+=max_right-height[right]
+                right-=1
+        return trapped_water
